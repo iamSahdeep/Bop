bb99902cf79d1c40478b6696a04771fc717b50b1
Sahdeep Singh <sahdeepsingh98@gmail.com>
2018-07-30 02:21:54 +0530

    handled permissions for api>23

--- app/src/main/java/com/sahdeepsingh/Bop/ui/MainScreen.java
+++ app/src/main/java/com/sahdeepsingh/Bop/ui/MainScreen.java
@@ -7,19 +7,26 @@ import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.preference.PreferenceManager;
 import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v13.app.FragmentPagerAdapter;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.Toolbar;
 import android.support.v8.renderscript.Allocation;
 import android.support.v8.renderscript.Element;
@@ -50,8 +57,12 @@ import com.squareup.picasso.Picasso;
 
 import java.io.File;
 
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
 
 public class MainScreen extends ActivityMaster implements MediaController.MediaPlayerControl, ActionBar.TabListener, FragmentSongs.OnListFragmentInteractionListener, FragmentPlaylist.OnListFragmentInteractionListener, FragmentGenre.OnListFragmentInteractionListener, FragmentAlbum.OnListFragmentInteractionListener {
+
+    private static final int PERMISSION_REQUEST_CODE = 200;
 
     public static final String BROADCAST_ACTION = "lol";
     static final int USER_CHANGED_THEME = 1;
@@ -126,6 +137,13 @@ public class MainScreen extends ActivityMaster implements MediaController.MediaP
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+        {
+            if (!checkPerm()){
+                requestPerm();
+            }
+        }
+
         slidingUpPanelLayout = findViewById(R.id.sliding_layout);
 
         toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -146,22 +164,73 @@ public class MainScreen extends ActivityMaster implements MediaController.MediaP
 
     }
 
+    private boolean checkPerm() {
+        int result = ContextCompat.checkSelfPermission(getApplicationContext(), READ_EXTERNAL_STORAGE);
+        int result1 = ContextCompat.checkSelfPermission(getApplicationContext(), WRITE_EXTERNAL_STORAGE);
+
+        return result == PackageManager.PERMISSION_GRANTED && result1 == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestPerm() {
+
+        ActivityCompat.requestPermissions(this, new String[]{READ_EXTERNAL_STORAGE,WRITE_EXTERNAL_STORAGE}, PERMISSION_REQUEST_CODE);
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_REQUEST_CODE:
+                if (grantResults.length > 0) {
+
+                    boolean ReadAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                    boolean WriteAccepted = grantResults[1] == PackageManager.PERMISSION_GRANTED;
+
+                    if (ReadAccepted && WriteAccepted)
+                        Snackbar.make(findViewById(R.id.sliding_layout),"Permission Granted, working on Player", Snackbar.LENGTH_SHORT).show();
+                    else {
+
+                        Snackbar.make(findViewById(R.id.sliding_layout), "Permission Denied, can't work on Player", Snackbar.LENGTH_SHORT).show();
+
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                            if (shouldShowRequestPermissionRationale(READ_EXTERNAL_STORAGE)) {
+                                showMessageOKCancel("You need to allow access to both the permissions",
+                                        new DialogInterface.OnClickListener() {
+                                            @Override
+                                            public void onClick(DialogInterface dialog, int which) {
+                                                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                                                    requestPermissions(new String[]{READ_EXTERNAL_STORAGE, WRITE_EXTERNAL_STORAGE},
+                                                            PERMISSION_REQUEST_CODE);
+                                                }
+                                            }
+                                        });
+                                return;
+                            }
+                        }
+
+                    }
+                }
+
+
+                break;
+        }
+    }
+
+
+    private void showMessageOKCancel(String message, DialogInterface.OnClickListener okListener) {
+        new AlertDialog.Builder(MainScreen.this)
+                .setMessage(message)
+                .setPositiveButton("OK", okListener)
+                .setNegativeButton("Cancel", null)
+                .create()
+                .show();
+    }
+
     private void setupViewPager(ViewPager viewPager) {
         mSectionsPagerAdapter = new SectionsPagerAdapter(getFragmentManager());
         viewPager.setAdapter(mSectionsPagerAdapter);
     }
 
-
-    /**
-     * Starts the background process of scanning the songs.
-     *
-     * @param forceScan If we should scan again. You should set
-     *                  this to true if you want to scan again
-     *                  the database.
-     *                  Otherwise, leave it `false` so we don't
-     *                  rescan the songs when this Activity
-     *                  is created again for some reason.
-     */
 
     @Override
     public void onListFragmentInteraction(int position, String type) {
