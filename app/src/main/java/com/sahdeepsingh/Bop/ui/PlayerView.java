2780aa8733c761e7d00e805f14bd39cea4dfd1f6
Sahdeep Singh <sahdeepsingh98@gmail.com>
2018-10-30 21:23:50 +0530

    minor changes

--- app/src/main/java/com/sahdeepsingh/Bop/ui/PlayerView.java
+++ app/src/main/java/com/sahdeepsingh/Bop/ui/PlayerView.java
@@ -1,13 +1,23 @@
 package com.sahdeepsingh.Bop.ui;
 
+import android.annotation.SuppressLint;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.text.format.DateUtils;
 import android.transition.Transition;
 import android.view.View;
 import android.view.Window;
-import android.widget.LinearLayout;
+import android.widget.MediaController;
 import android.widget.TextView;
 
 import com.andremion.music.MusicCoverView;
@@ -16,14 +26,31 @@ import com.sahdeepsingh.Bop.playerMain.Main;
 import com.sahdeepsingh.Bop.view.ProgressView;
 import com.sahdeepsingh.Bop.view.TransitionAdapter;
 
-public class PlayerView extends AppCompatActivity {
+import java.io.File;
+
+import static com.sahdeepsingh.Bop.ui.MainScreen.BROADCAST_ACTION;
+
+public class PlayerView extends AppCompatActivity implements MediaController.MediaPlayerControl {
 
     private MusicCoverView mCoverView;
-    private LinearLayout mTitleView;
+    @SuppressLint("HandlerLeak")
+    private final Handler mUpdateProgressHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            final int position = getCurrentPosition() / 1000;
+            final int duration = (int) Main.musicService.currentSong.getDurationSeconds();
+            onUpdateProgress(position, duration);
+            sendEmptyMessageDelayed(0, DateUtils.SECOND_IN_MILLIS);
+        }
+    };
     private FloatingActionButton mFabView;
     private TextView mTimeView;
     private TextView mDurationView;
     private ProgressView mProgressView;
+    ChangeSongBR changeSongBR;
+    private TextView mTitleView;
+    private boolean paused = false;
+    private boolean playbackPaused = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,8 +60,8 @@ public class PlayerView extends AppCompatActivity {
         supportRequestWindowFeature(Window.FEATURE_ACTION_BAR_OVERLAY);
         setContentView(R.layout.playerview);
 
-        mCoverView = (MusicCoverView) findViewById(R.id.cover);
-        mTitleView = findViewById(R.id.title);
+        mCoverView = findViewById(R.id.cover);
+        mTitleView = findViewById(R.id.titleTrack);
         mTimeView = findViewById(R.id.time);
         mDurationView = findViewById(R.id.duration);
         mProgressView = findViewById(R.id.progress);
@@ -58,9 +85,186 @@ public class PlayerView extends AppCompatActivity {
                 mCoverView.start();
             }
         });
+        changeSongBR = new ChangeSongBR();
+
     }
 
     public void onFabClick(View view) {
         mCoverView.stop();
     }
+
+    private void workOnImages() {
+        File path = null;
+        if (Main.songs.getAlbumArt(Main.musicService.currentSong) != null)
+            path = new File(Main.songs.getAlbumArt(Main.musicService.currentSong));
+        Bitmap bitmap;
+        if (path != null && path.exists()) {
+            bitmap = BitmapFactory.decodeFile(path.getAbsolutePath());
+        } else bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.back);
+        mCoverView.setImageBitmap(bitmap);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(changeSongBR);
+        paused = true;
+        playbackPaused = true;
+    }
+
+    /**
+     * Activity has become visible.
+     */
+    @Override
+    protected void onResume() {
+        super.onResume();
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(BROADCAST_ACTION);
+        registerReceiver(changeSongBR, intentFilter);
+        Main.musicService.notifyCurrentSong();
+        if (paused) {
+            paused = false;
+        }
+    }
+
+    /**
+     * Activity is no longer visible.
+     */
+
+    @Override
+    public void start() {
+        Main.musicService.unpausePlayer();
+    }
+
+    /**
+     * Callback to when the user pressed the `pause` button.
+     */
+    @Override
+    public void pause() {
+        Main.musicService.pausePlayer();
+    }
+
+    @Override
+    public int getDuration() {
+        if (Main.musicService != null && Main.musicService.musicBound
+                && Main.musicService.isPlaying())
+            return Main.musicService.getDuration();
+        else
+            return 0;
+    }
+
+    @Override
+    public int getCurrentPosition() {
+        if (Main.musicService != null && Main.musicService.musicBound
+                && Main.musicService.isPlaying())
+            return Main.musicService.getPosition();
+        else
+            return 0;
+    }
+
+    @Override
+    public void seekTo(int position) {
+        Main.musicService.seekTo(position);
+    }
+
+    @Override
+    public boolean isPlaying() {
+        if (Main.musicService != null && Main.musicService.musicBound)
+            return Main.musicService.isPlaying();
+
+        return false;
+    }
+
+    @Override
+    public int getBufferPercentage() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public boolean canPause() {
+        return true;
+    }
+
+    @Override
+    public boolean canSeekBackward() {
+        return true;
+    }
+
+    @Override
+    public boolean canSeekForward() {
+        return true;
+    }
+
+    @Override
+    public int getAudioSessionId() {
+        return Main.musicService.getAudioSession();
+    }
+
+    // Back to the normal methods
+
+    /**
+     * Jumps to the next song and starts playing it right now.
+     */
+    public void playNext() {
+        Main.musicService.next(true);
+        Main.musicService.playSong();
+
+        // To prevent the MusicPlayer from behaving
+        // unexpectedly when we pause the song playback.
+        if (playbackPaused) {
+            playbackPaused = false;
+        }
+
+/*
+        musicController.show();
+*/
+    }
+
+    /**
+     * Jumps to the previous song and starts playing it right now.
+     */
+    public void playPrevious() {
+        Main.musicService.previous(true);
+        Main.musicService.playSong();
+
+        // To prevent the MusicPlayer from behaving
+        // unexpectedly when we pause the song playback.
+        if (playbackPaused) {
+            playbackPaused = false;
+        }
+
+/*
+        musicController.show();
+*/
+    }
+
+    private void onUpdateProgress(int position, int duration) {
+        if (mTimeView != null) {
+            mTimeView.setText(DateUtils.formatElapsedTime(position));
+        }
+        if (mDurationView != null) {
+            mDurationView.setText(DateUtils.formatElapsedTime(duration));
+        }
+        if (mProgressView != null) {
+            mProgressView.setProgress(position);
+        }
+    }
+
+    class ChangeSongBR extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            mUpdateProgressHandler.sendEmptyMessage(0);
+            mTitleView.setText(Main.musicService.currentSong.getTitle());
+            mTitleView.setSelected(true);
+            workOnImages();
+            if (!Main.musicService.isPaused()) {
+                mFabView.setImageResource(R.mipmap.ic_pause);
+            } else {
+                mFabView.setImageResource(R.mipmap.ic_play);
+            }
+        }
+    }
+
 }
